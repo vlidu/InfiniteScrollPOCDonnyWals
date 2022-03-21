2e0d09735a8170ae70ad85083a6c8f6867c419f9
Alexandre VLADOVICH-RELJA <avladovichrelja@sqli.com>
2022-03-21 15:25:43 +0100

    Initial Commit

---
+++ InfiniteScrollSwiftUI/InfiniteScrollSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollSwiftUIApp.swift
+//  InfiniteScrollSwiftUI
+//
+//  Created by Alexandre VLADOVICH RELJA on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
